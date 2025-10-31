bdb0b1f1e9054c75bc6893a20a1cd9c1a2f00582
javiiariass <javierariasfuentes@gmail.com>
2025-10-31 18:42:31 +0100

    check pa usar pc Grande

---
+++ src/java/daw/model/Recipe.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author Javi
+ */
+@Entity
+@Table(name = "recipes")
+@NamedQueries({
+    @NamedQuery(name = "Recipe.findAll", query = "SELECT r FROM Recipe r"),
+    @NamedQuery(name = "Recipe.findByName", query = "SELECT r FROM Recipe r WHERE r.name = :name")
+})
+public class Recipe implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    // ******************* Atributos *******************
+    // PrimaryKey of DB
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    String titulo;
+    String descripcion;
+    String instrucciones;
+    int tiempoMinutos;
+    int porciones;
+    double calorias; //permite ser nulo
+    double proteinas; //permite ser nulo
+    double grasas; //permite ser nulo
+    double hidratos; //permite ser nulo
+    String rutaImagen;
+    boolean publica;
+    Date createdAt;
+    Date updatedAt;
+    List<Ingredient> ingredients;
+    
+    // ******************* Getters & Setters *******************
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Recipe)) {
+            return false;
+        }
+        Recipe other = (Recipe) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "daw.model.Recipe[ id=" + id + " ]";
+    }
+    
+}
